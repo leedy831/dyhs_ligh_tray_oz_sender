8616764a072d0c085bbecd122ff538feac265772
leedy831 <67949582+leedy831@users.noreply.github.com>
2022-12-07 14:18:31 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(-1)
+})
+radio.setGroup(33)
